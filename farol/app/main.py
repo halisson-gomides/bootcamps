4ab5ca9a479334c25fc0231e844aa2f6c1055fac
Halisson Gomides <halisson.gomides@gmail.com>
2024-09-26 18:58:22 -0300

    aula-farol

--- farol/app/main.py
+++ farol/app/main.py
@@ -1,7 +1,7 @@
 from nicegui import ui, app
 
 app.add_static_files(url_path="/img", local_directory="img")
-
+ui.markdown.default_classes('text-lg')
 @ui.page('/')
 def principal()->None:
     with open('README.md', 'r', encoding='utf-8') as f:
